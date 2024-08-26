4f8393b68bcbc8ec824cd6b14c376770e8b4039b
Jeewon Park <yuha8486@gmail.com>
2024-08-26 13:35:04 +0900

    콘다 커맨드 실행기

--- Utils.cs
+++ Utils.cs
@@ -37,5 +37,60 @@ namespace Ophiuchus
             return proc;
         }
 
+        public static async Task RunCommandWithVisibleCmd(string command, params string[] args)
+        {
+            string arguments = $"{command} {string.Join(" ", args.Select(arg => arg.Contains(" ") ? $"\"{arg}\"" : arg))}";
+
+            using (var process = new Process())
+            {
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = $"/C {VARS.CMD_CONDA} {arguments} || pause";
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.CreateNoWindow = false;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+
+                process.Start();
+                await Task.Run(() => process.WaitForExit());
+            }
+        }
+
+        public static async Task RunCommandWithOutput(string command, string arguments)
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo.FileName = command;
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.OutputDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        // 여기서 UI에 출력을 표시합니다.
+                        // 예: textBox.AppendText(e.Data + Environment.NewLine);
+                        Console.WriteLine(e.Data);
+                    }
+                };
+
+                process.ErrorDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        // 여기서 UI에 에러를 표시합니다.
+                        // 예: textBox.AppendText("Error: " + e.Data + Environment.NewLine);
+                        Console.WriteLine("Error: " + e.Data);
+                    }
+                };
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                await Task.Run(() => process.WaitForExit());
+            }
+        }
     }
 }
