ab4b669ef09d4b53b441775418b8500369ae492b
Jeewon Park <yuha8486@gmail.com>
2024-08-26 13:35:31 +0900

    누가 만들었는지 보여주는 거

---
+++ Whomade.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ophiuchus
+{
+    /// <summary>
+    /// Whomade.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Whomade : Window
+    {
+        public Whomade()
+        {
+            InitializeComponent();
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(e.Uri.AbsoluteUri)
+            {
+                UseShellExecute = true
+            });
+            e.Handled = true;
+        }
+        private void OnClickClose(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+
+}
